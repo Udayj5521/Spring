c41f2284c7905a7b18d92c8e05d73cb684795b6b
Udayj5521 <106623042+Udayj5521@users.noreply.github.com>
2023-01-18 08:32:10 +0530

    CollectionList

---
+++ SpringCollectionList/src/main/java/com/Address.java
@@ -0,0 +1,19 @@
+package com;
+
+import java.util.List;
+
+public class Address {
+	
+	List addressList;
+
+	public List getAddressList() {
+		return addressList;
+	}
+
+	public void setAddressList(List addressList) {
+		this.addressList = addressList;
+	}
+	
+	
+
+}
---
+++ SpringCollectionList/src/main/java/com/TestSaveAddress.java
@@ -0,0 +1,16 @@
+package com;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSaveAddress {
+	public static void main(String[] args) {
+
+		ApplicationContext context = new ClassPathXmlApplicationContext("AddressBean.xml");
+
+		Address a = (Address) context.getBean("myAddress");
+
+		System.out.println("List Elements: " + a.getAddressList());
+	}
+
+}
