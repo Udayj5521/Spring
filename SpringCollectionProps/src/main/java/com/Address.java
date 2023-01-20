7bfcf71a3f34ca18619a544eaf50ff8a71174ce3
Udayj5521 <106623042+Udayj5521@users.noreply.github.com>
2023-01-20 09:11:40 +0530

    Collection Properties

---
+++ SpringCollectionProps/src/main/java/com/Address.java
@@ -0,0 +1,17 @@
+package com;
+
+import java.util.Properties;
+
+public class Address {
+
+	Properties addressProp;
+
+	public Properties getAddressProp() {
+		return addressProp;
+	}
+
+	public void setAddressProp(Properties addressProp) {
+		this.addressProp = addressProp;
+	}
+
+}
