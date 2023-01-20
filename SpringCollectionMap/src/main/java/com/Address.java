363b90f7289055ae021df7de0f5108ae38f354e8
Udayj5521 <106623042+Udayj5521@users.noreply.github.com>
2023-01-20 09:10:04 +0530

    CollectionMap

---
+++ SpringCollectionMap/src/main/java/com/Address.java
@@ -0,0 +1,17 @@
+package com;
+
+import java.util.Map;
+
+public class Address {
+
+	Map addressMap;
+
+	public Map getAddressMap() {
+		return addressMap;
+	}
+
+	public void setAddressMap(Map addressMap) {
+		this.addressMap = addressMap;
+	}
+
+}
